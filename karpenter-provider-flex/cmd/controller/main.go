ad199e1a7998590f43400508e140fce226b38793
hbc <me@hbc.rocks>
2026-02-20 21:10:53 -0800

    refactor: move to sub folder

---
+++ karpenter-provider-flex/cmd/controller/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/Azure/karpenter-provider-azure/pkg/apis"
+	"github.com/Azure/karpenter-provider-azure/pkg/cloudprovider"
+	"github.com/Azure/karpenter-provider-azure/pkg/controllers"
+	"github.com/Azure/karpenter-provider-azure/pkg/operator"
+	"github.com/Azure/karpenter-provider-azure/pkg/operator/options"
+	"github.com/go-logr/zapr"
+	"github.com/samber/lo"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/karpenter/pkg/cloudprovider/metrics"
+	"sigs.k8s.io/karpenter/pkg/cloudprovider/overlay"
+	corecontrollers "sigs.k8s.io/karpenter/pkg/controllers"
+	"sigs.k8s.io/karpenter/pkg/controllers/state"
+	coreoperator "sigs.k8s.io/karpenter/pkg/operator"
+	"sigs.k8s.io/karpenter/pkg/operator/injection"
+	"sigs.k8s.io/karpenter/pkg/operator/logging"
+	coreoptions "sigs.k8s.io/karpenter/pkg/operator/options"
+
+	"github.com/Azure/aks-flex/karpenter-provider-flex/pkg/apis/v1alpha1"
+	flexcloudproviders "github.com/Azure/aks-flex/karpenter-provider-flex/pkg/cloudproviders"
+	"github.com/Azure/aks-flex/karpenter-provider-flex/pkg/cloudproviders/nebius"
+	flexcontrollers "github.com/Azure/aks-flex/karpenter-provider-flex/pkg/controllers"
+	flexoptions "github.com/Azure/aks-flex/karpenter-provider-flex/pkg/options"
+	utilsk8s "github.com/Azure/aks-flex/karpenter-provider-flex/pkg/utils/k8s"
+	wireguard "github.com/Azure/aks-flex/karpenter-provider-flex/pkg/utils/wireguard"
+)
+
+func init() {
+	// FIXME: review this logic... are we sure this is the right way?
+	v1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme)
+}
+
+func main() {
+	ctx := injection.WithOptionsOrDie(context.Background(), coreoptions.Injectables...)
+	logger := zapr.NewLogger(logging.NewLogger(ctx, "controller"))
+	lo.Must0(
+		operator.WaitForCRDs(
+			ctx, 2*time.Minute, ctrl.GetConfigOrDie(), logger,
+			&v1alpha1.NebiusNodeClass{},
+		),
+		"failed waiting for CRDs",
+	)
+
+	ctx, op := operator.NewOperator(coreoperator.NewOperator())
+
+	// TODO: Consider also dumping at least some core options
+	logger.V(0).Info("Initial options", "options", options.FromContext(ctx).String())
+
+	flexoptions.MustInitalizeStretchPlugin(ctx, op.GetConfig())
+
+	hubCloudProvider := flexcloudproviders.NewCloudProvidersHub()
+	defer func() {
+		if err := hubCloudProvider.Close(ctx); err != nil {
+			logger.Error(err, "closing cloud providers")
+		}
+	}()
+
+	// AKS cloud provider...
+	var aksCloudProvider *cloudprovider.CloudProvider
+	{
+		aksCloudProvider = cloudprovider.New(
+			op.InstanceTypesProvider,
+			op.VMInstanceProvider,
+			op.AKSMachineProvider,
+			op.EventRecorder,
+			op.GetClient(),
+			op.ImageProvider,
+			op.InstanceTypeStore,
+		)
+		lo.Must0(op.AddHealthzCheck("cloud-provider", aksCloudProvider.LivenessProbe))
+		hubCloudProvider.Register(aksCloudProvider, schema.GroupKind{
+			Group: apis.Group,
+			Kind:  "AKSNodeClass",
+		}, "azure")
+	}
+
+	clusterCA := lo.Must(utilsk8s.RetrieveClusterCA(op.GetConfig()))
+
+	// nebius cloud provider...
+	{
+		wgAlloc := wireguard.NewIPAllocator(op.Manager.GetCache(), nebius.GroupKind, 30*time.Second)
+		defer wgAlloc.Close()
+
+		err := nebius.Register(
+			ctx,
+			hubCloudProvider,
+			flexoptions.MustNewNebiusSDK(ctx),
+			op.GetClient(),
+			clusterCA,
+			wgAlloc,
+		)
+		lo.Must0(err, "registering nebius cloud provider")
+	}
+
+	overlayUndecoratedCloudProvider := metrics.Decorate(hubCloudProvider)
+	cloudProvider := overlay.Decorate(overlayUndecoratedCloudProvider, op.GetClient(), op.InstanceTypeStore)
+	clusterState := state.NewCluster(op.Clock, op.GetClient(), cloudProvider)
+
+	op.
+		WithControllers(ctx, corecontrollers.NewControllers(
+			ctx,
+			op.Manager,
+			op.Clock,
+			op.GetClient(),
+			op.EventRecorder,
+			cloudProvider,
+			overlayUndecoratedCloudProvider,
+			clusterState,
+			op.InstanceTypeStore,
+		)...).
+		WithControllers(ctx, controllers.NewControllers(
+			ctx,
+			op.Manager,
+			op.GetClient(),
+			op.EventRecorder,
+			aksCloudProvider,
+			op.VMInstanceProvider,
+			op.AKSMachineProvider,
+			// TODO: still need to refactor ImageProvider side of things.
+			op.KubernetesVersionProvider,
+			op.ImageProvider,
+			op.InClusterKubernetesInterface,
+			op.AZClient.SubnetsClient(),
+		)...).
+		WithControllers(ctx, flexcontrollers.NewControllers(
+			ctx,
+			op.GetClient(),
+			op.EventRecorder,
+		)...).
+		Start(ctx)
+}
--- pkg/apis/v1alpha1/labels.go
+++
@@ -1,7 +0,0 @@
-package v1alpha1
-
-import "github.com/Azure/karpenter-provider-flex/pkg/apis"
-
-const (
-	TerminationFinalizer = apis.Group + "/termination"
-)
